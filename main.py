e90358569f1df07351787b16f90b5993a2808a78
chrxmo <mishanya.ru53@gmail.com>
2024-11-14 00:09:23 +0500

    add main file

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+
+
+from app.services.vision_service import GetTextFromImage
+
+app = FastAPI()
+
+
+@app.post('/upload_image')
+async def upload_image(file: UploadFile = File(...)):
+    if file.content_type not in ['image/jpeg', 'image/png']:
+        return JSONResponse(content={'Ошибка': 'Неправильный формат файла.'}, status_code=400)
+
+    image_data = await file.read()
+
+    text_composition = GetTextFromImage(image_data)
+
+    return {'Состав:': text_composition}
