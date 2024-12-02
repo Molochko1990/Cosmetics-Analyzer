914730c91ecc39839863f393bad41131cb1dab1c
Filipp16 <gaev.f@bk.ru>
2024-12-02 20:09:07 +0500

    соединили fastapi и front

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Form
 from fastapi.responses import JSONResponse
-
+from starlette.middleware.cors import CORSMiddleware
 
 from app.services.vision_service import GetTextFromImage
 
 app = FastAPI()
+
+# Настройка CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Разрешить запросы с любых доменов
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешить все методы (GET, POST и т.д.)
+    allow_headers=["*"],  # Разрешить все заголовки
+)
 
 
 @app.post('/upload_image')
@@ -14,6 +23,12 @@ async def upload_image(file: UploadFile = File(...)):
 
     image_data = await file.read()
 
-    text_composition = GetTextFromImage(0, image_data)
+    text_composition = GetTextFromImage(1, image_data)
 
-    return {'Состав:': text_composition}
+    return {'Состав': text_composition}
+
+
+@app.post("/submit-text/")
+async def submit_text(text: str = Form(...)):
+    text_composition = text
+    return JSONResponse(content={"Состав": text_composition})
