89642878588354065c0a920956d06e01dce0ec2d
Кирилл <41121179+kukhvatov@users.noreply.github.com>
2024-12-08 21:47:08 +0500

    Некоторые тех. изменеия

--- app/services/cosmetic_risk_determining_service.py
+++ app/services/cosmetic_risk_determining_service.py
@@ -5,7 +5,7 @@ URL = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
 
 def determining_cosmetic_risk(ids):
     ingredients = get_latin_name(ids)
-    iam_token = "t1.9euelZrHxs-Sxo2dl8eYlY-LnZmMyu3rnpWakI_Pz5rJl5Sbyp2ek8iSyZrl8_dlJClF-e9Bdzcw_d3z9yVTJkX570F3NzD9zef1656VmpHHx8eUkYyOipuRlJGRxouL7_zF656VmpHHx8eUkYyOipuRlJGRxouL.FP9ERztxH7iSLqhFegr81OAPEAzuCszyUb0_c0jYxR_Z1uqcNnmSka6cPEhDslsJrrUlNmQvmBoYRsbOJk5YCw"
+    iam_token = ""
     folder_id = "b1gf3amjf6uujcqkj28e"
     data = {}
     data["modelUri"] = f"gpt://{folder_id}/yandexgpt-lite/latest"
